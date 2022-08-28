67fbdf57b1e7012df843c1c80e12af85cd8b1bf2
Ramon Fontes <ramonreisfontes@gmail.com>
2022-08-28 12:37:48 -0300

    add my-module.py

---
+++ codes/cap4/my-module.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+'This example creates a simple network topology with 1 AP and 2 stations'
+
+import sys
+
+from mininet.log import setLogLevel, info
+from mn_wifi.cli import CLI
+from mn_wifi.net import Mininet_wifi
+
+
+def topology():
+    "Create a network."
+    net = Mininet_wifi()
+
+    info("*** Creating nodes\n")
+    ap1 = net.addAccessPoint('ap1', ssid="simpletopo", mode="g", channel="5")
+    sta1 = net.addStation('sta1')
+    sta2 = net.addStation('sta2')
+
+    info("*** Starting a new module\n")
+    net.setModule('./mac80211_hwsim.ko')
+
+    info("*** Configuring wifi nodes\n")
+    net.configureWifiNodes()
+
+    info("*** Associating Stations\n")
+    net.addLink(sta1, ap1)
+    net.addLink(sta2, ap1)
+
+    info("*** Starting network\n")
+    net.build()
+    ap1.start([])
+
+    info("*** Running CLI\n")
+    CLI(net)
+
+    info("*** Stopping network\n")
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topology()
